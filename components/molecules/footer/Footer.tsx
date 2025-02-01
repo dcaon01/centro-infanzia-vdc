34acbb006fc01964b0725f099a4bcf4693a24fe4
dcaon01 <d.caon01@gmail.com>
2025-02-01 21:28:04 +0100

    Footer implemented

--- components/molecules/footer/Footer.tsx
+++ components/molecules/footer/Footer.tsx
@@ -3,14 +3,31 @@ import classes from "./Footer.module.scss";
 const Footer = () => {
     return (
         <div className={classes.footerContainer}>
-            <div className={classes.footerComponent}>
-                <h3>Indirizzo</h3>
+            <div className={classes.footerInfo}>
+                <div className={classes.footerInfoComponent}>
+                    <h3>Indirizzo</h3>
+                    <div className={classes.footerInfoComponentContent}>
+                        <p>Via Roma, 69, 35010</p>
+                        <p>Villa del Conte PD</p>
+                    </div>
+                </div>
+                <div className={`${classes.footerInfoComponent} ${classes.central}`}>
+                    <h3>Contatti</h3>
+                    <div className={classes.footerInfoComponentContent}>
+                        <p>Telefono: 049 574 4064</p>
+                        <p>Email: asilo@email.com</p>
+                    </div>
+                </div>
+                <div className={classes.footerInfoComponent}>
+                    <h3>Social</h3>
+                    <div className={classes.footerInfoComponentContent}>
+                        <p>Telefono: 049 574 4064</p>
+                        <p>Email: asilo@email.com</p>
+                    </div>
+                </div>
             </div>
-            <div className={classes.footerComponent}>
-                <h3>Contatti</h3>
-            </div>
-            <div className={classes.footerComponent}>
-                <h3>Social</h3>
+            <div className={classes.anchor}>
+                <p>Copyright &#169; 2025 Parrocchia di Villa del Conte - Credits by Damiano Caon</p>
             </div>
         </div>
     );
