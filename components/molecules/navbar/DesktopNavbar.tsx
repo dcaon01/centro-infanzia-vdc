654806134747d02463fd154acc4418270cf41c73
dcaon01 <d.caon01@gmail.com>
2025-02-02 22:52:37 +0100

    Navbar functionality fixed

---
+++ components/molecules/navbar/DesktopNavbar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import classes from "./DesktopNavbar.module.scss";
+
+/**
+ * Desktop navbar.
+ * @returns the desktop navbar render.
+ */
+const DesktopNavbar = () => {
+    return (
+        <div 
+            className={classes.desktopNavbarContainer}
+        >
+            <nav className={classes.desktopNavbar}>
+                <Link className={`${classes.link} ${classes.left}`} href={"/"}>Home</Link>
+                <Link className={classes.link} href={"/servizi"}>Servizi</Link>
+                <Link className={classes.link} href={"/progettazione"}>Progettazione</Link>
+                <Link className={classes.link} href={"/regolamento"}>Regolamento</Link>
+                <Link className={classes.link} href={"/menu"}>Menù</Link>
+                <Link className={classes.link} href={"/modulistica"}>Modulistica</Link>
+                <Link className={`${classes.link} ${classes.right}`} href={"/contatti"}>Contatti</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default DesktopNavbar;
