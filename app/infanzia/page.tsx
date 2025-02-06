57575934c6ce3cfaa9d646f224501b28d529e98f
dcaon01 <d.caon01@gmail.com>
2025-02-07 00:01:13 +0100

    Educational offer page implemented

--- app/infanzia/page.tsx
+++ app/infanzia/page.tsx
@@ -37,6 +37,31 @@ const ChildhoodPage = () => {
         <main>
             <PageHeader title="Infanzia" />
             <div className={classes.pageContent}>
+                <h2>Cos'è la scuola d'infanzia?</h2>
+                <p>La scuola d’infanzia si rivolge a tutti i bambini e bambine di età compresa tra i tre e i sei anni.</p>
+                <p>
+                    La scuola dell’infanzia concorre all’educazione e allo sviluppo affettivo, sociale, psicomotorio,
+                    cognitivo e religioso dei bambini promuovendone le potenzialità di relazione, autonomia,
+                    creatività, apprendimento e mira ad assicurare un’effettiva uguaglianza delle opportunità
+                    educative. Nel rispetto del ruolo educativo dei genitori, contribuisce alla formazione integrale dei
+                    bambini e realizza la continuità educativa con il nido e la scuola primaria.
+                </p>
+                <p>
+                    Nella scuola dell’infanzia l’apprendimento avviene attraverso l’esplorazione, il contatto con gli
+                    oggetti, la natura, l’arte, il territorio in una dimensione ludica.
+                    Le metodologie didattiche fanno riferimento all’esperienza concreta, all’esplorazione, alla
+                    scoperta, al gioco, al procedere per tentativi ed errori, alla conversazione e al confronto tra pari e
+                    con l’adulto. Molto importanti sono le routine che regolano i ritmi della giornata e aiutano i
+                    bambini ad orientarsi rispetto allo scorrere del tempo e potenziano le loro competenze personali,
+                    cognitive, affettive e comunicative. Ampio spazio viene riservato al gioco durante il quale i bambini
+                    si esprimono, raccontano, rielaborano in modo creativo le esperienze personali e sociali.
+                </p>
+                <p>
+                    L’osservazione da parte dei docenti rappresenta uno strumento fondamentale per accompagnare
+                    il bambino nel suo sviluppo integrale; la documentazione serve a tenere traccia dei progressi
+                    dell’apprendimento individuale e di gruppo; la verifica documenta i processi di crescita di ciascun
+                    bambini.
+                </p>
                 <Masonry
                     breakpointCols={{ default: 3, 768: 2, 480: 2 }}
                     className={classes.masonryGrid}
